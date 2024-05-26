c26570368328131d6c9c72aa022d49f1da2b8609
Stanislav Popov <popstas@gmail.com>
2024-05-26 15:57:29 +0500

    feat: env, list, link

--- ansible-server.plugin.zsh
+++ ansible-server.plugin.zsh
@@ -2,13 +2,23 @@
 
 ANSIBLE_SERVER_PATH="${ANSIBLE_SERVER_PATH:-.}"
 
+exclude_regexp="(_examples|_old|_disabled)"
+
 __as_not_found_msg () {
     echo 'Define $ANSIBLE_SERVER_PATH or use from ansible-server root'
 }
 
+__as_path () {
+    if [ -d "$ANSIBLE_SERVER_PATH/build" ]; then
+        echo "$ANSIBLE_SERVER_PATH/build"
+    else
+        echo "$ANSIBLE_SERVER_PATH"
+    fi
+}
+
 __as_ll_group_list () {
     # parses the ini hostfile for groups only: [...]
-    HOST_FILE="${ANSIBLE_SERVER_PATH}/hosts_all"
+    HOST_FILE="$(__as_path)/hosts_all"
     if [ ! -e "$HOST_FILE" ]; then
         return
     fi
@@ -24,18 +34,18 @@ __as_ll_group_list () {
 }
 
 __as_asite_tags_list () {
-    _values  -s , apache cron dns letsencrypt nginx php ssh-keys sync sync_files sync_mysql
+    _values  -s , apache,nginx,php apache cron dns letsencrypt nginx php ssh-keys sync sync_files sync_mysql
 }
 
 __as_aproject_tags_list () {
-    _values  -s , cron dns ssl nginx ssh-keys docker deploy config
+    _values  -s , config,deploy cron dns ssl nginx ssh-keys docker deploy config
 }
 
 __as_host_list ()
 {
     # parses the ini hostfile for hosts only
     # but then has to remove all group occurrences
-    HOST_FILE="${ANSIBLE_SERVER_PATH}/hosts_all"
+    HOST_FILE="$(__as_path)/hosts_all"
     if [ ! -e "$HOST_FILE" ]; then
         return
     fi
@@ -77,7 +87,7 @@ _ansible_host_complete () {
         hosts)
             local -a results
             #_message -r "$(__as_not_found_msg)" # always show
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/playbooks/hosts"
+            local playbooks_path="$(__as_path)/playbooks/hosts"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
                 results=( $(find "$playbooks_path/" -name '*.yml' | sed "s/$playbooks_path_sed//g" | sed 's/\.yml$//g' | sort) )
@@ -104,7 +114,7 @@ _ansible_deploy_complete () {
         deploy)
             local -a results
             _message -r "$(__as_not_found_msg)"
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/playbooks/deploy"
+            local playbooks_path="$(__as_path)/playbooks/deploy"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
                 results=( $(find "$playbooks_path" -name '*.yml' | sed "s/$playbooks_path_sed//g" | sed 's/\.yml$//g') )
@@ -131,7 +141,7 @@ _ansible_role_complete () {
             ;;
         roles)
             local -a results
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/playbooks/roles"
+            local playbooks_path="$(__as_path)/playbooks/roles"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
                 results=( $(find "$playbooks_path" -name '*.yml' | sed "s/$playbooks_path_sed//g" | sed 's/\.yml$//g') )
@@ -160,7 +170,7 @@ _ansible_site_complete () {
             ;;
         sites)
             local -a results
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/vars/sites/hosts"
+            local playbooks_path="$(__as_path)/vars/sites/hosts"
             # echo "playbooks_path: $playbooks_path"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
@@ -170,6 +180,37 @@ _ansible_site_complete () {
             else
                 _message -r "$(__as_not_found_msg)"
             fi
+            ;;
+    esac
+}
+
+_ansible_env_complete () {
+    _arguments -C \
+    "1:first_arg:->env" \
+
+    case "$state" in
+        env)
+            local -a results
+            local config_path="${HOME}/.ansible-server.yml"
+            if [ -e "$config_path" ]; then
+                results=( $(cat "$config_path" | yq '.env[].name') )
+                _values 'results' $results
+            else
+                _message -r "$(__as_not_found_msg)"
+            fi
+            ;;
+    esac
+}
+
+_ansible_list_complete () {
+    _arguments -C \
+    "1:first_arg:->list" \
+
+    case "$state" in
+        list)
+            local -a results
+            results=( host project site )
+            _values 'results' $results
             ;;
     esac
 }
@@ -188,7 +229,7 @@ _ansible_project_complete () {
             ;;
         projects)
             local -a results
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/vars/projects/hosts"
+            local playbooks_path="$(__as_path)/vars/projects/hosts"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
                 results=( $(find "$playbooks_path" -name '*.yml' | sed "s/$playbooks_path_sed//g" | sed 's/\.yml$//g') )
@@ -220,10 +261,31 @@ _ansible_sites_foreach_complete () {
     case "$state" in
         groups)
             local -a results
-            local playbooks_path="${ANSIBLE_SERVER_PATH}/vars/sites/hosts"
+            local playbooks_path="$(__as_path)/vars/sites/hosts"
             if [ -d "$playbooks_path" ]; then
                 local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
-                results=( $(find "$playbooks_path" -type d -mindepth 1 | grep -v '_examples' | sed "s/$playbooks_path_sed//g") )
+                results=( $(find "$playbooks_path" -mindepth 1 -type d | grep -vE "$exclude_regexp" | sed "s/$playbooks_path_sed//g") )
+                _values 'results' $results
+                _values 'results' all
+                #_multi_parts -i / results # buggy
+            else
+                _message -r "$(__as_not_found_msg)"
+            fi
+            ;;
+    esac
+}
+
+_ansible_projects_foreach_complete () {
+    _arguments -C \
+    "1:first_arg:->groups"
+
+    case "$state" in
+        groups)
+            local -a results
+            local playbooks_path="$(__as_path)/vars/projects/hosts"
+            if [ -d "$playbooks_path" ]; then
+                local playbooks_path_sed="$(echo "$playbooks_path/" | sed 's|\/|\\\/|g')"
+                results=( $(find "$playbooks_path" -mindepth 1 -type d | grep -vE "$exclude_regexp" | sed "s/$playbooks_path_sed//g") )
                 _values 'results' $results
                 _values 'results' all
                 #_multi_parts -i / results # buggy
@@ -241,7 +303,7 @@ _ansible_server_complete () {
 
     case "$state" in
         (command)
-            _values 'results' role deploy host site
+            _values 'results' role deploy host site project env link list
             ;;
         (option-or-argument)
             curcontext=${curcontext%:*:*}:ansible-server-$words[1]:
@@ -267,22 +329,35 @@ _ansible_server_subcommand () {
         (site)
             _ansible_site_complete
             ;;
+        (env)
+            _ansible_env_complete
+            ;;
+        (list)
+            _ansible_list_complete
+            ;;
     esac
 }
 
 compdef _ansible_server_complete ansible-server
 compdef _ansible_host_complete ansible-host
 compdef _ansible_deploy_complete ansible-deploy
-compdef _ansible_project_complete ansible-project
 compdef _ansible_role_complete ansible-role
 compdef _ansible_site_complete ansible-site
+compdef _ansible_project_complete ansible-project
+compdef _ansible_env_complete ansible-env
+compdef _ansible_list_complete ansible-list
 compdef _ansible_shell_complete ansible-shell
 compdef _ansible_sites_foreach_complete sites-foreach
+compdef _ansible_projects_foreach_complete projects-foreach
 
 alias ahost='ansible-server host'
 alias adeploy='ansible-server deploy'
-alias aproject='ansible-server project'
 alias arole='ansible-server role'
 alias asite='ansible-server site'
+alias aproject='ansible-server project'
+alias aenv='ansible-server env'
+alias alink='ansible-server link'
 alias aforeach=sites-foreach
+alias asites=sites-foreach
+alias aprojects=projects-foreach
 alias ashell=ansible-shell
